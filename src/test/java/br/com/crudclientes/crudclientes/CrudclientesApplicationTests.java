f3875bc0228a470775dfced52fee7c1c84b921fc
Gilnario Saraiva <gilnariosaraiva@gmail.com>
2024-07-17 20:22:14 -0300

    Iniciando repositório local

---
+++ src/test/java/br/com/crudclientes/crudclientes/CrudclientesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.crudclientes.crudclientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudclientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
