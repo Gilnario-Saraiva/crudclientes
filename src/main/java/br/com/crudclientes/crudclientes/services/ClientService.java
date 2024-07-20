5f0e4351257dc42c181cc113515eca1b70827a70
Gilnario Saraiva <gilnariosaraiva@gmail.com>
2024-07-19 21:27:29 -0300

    busca paginada

--- src/main/java/br/com/crudclientes/crudclientes/services/ClientService.java
+++ src/main/java/br/com/crudclientes/crudclientes/services/ClientService.java
@@ -4,9 +4,12 @@ import br.com.crudclientes.crudclientes.dto.ClientDTO;
 import br.com.crudclientes.crudclientes.entities.Client;
 import br.com.crudclientes.crudclientes.repositories.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -17,5 +20,11 @@ public class ClientService {
     public ClientDTO findById(Long id){
         Client client = repository.findById(id).get();
         return new ClientDTO(client);
+    }
+
+    @Transactional(readOnly = true)
+    public Page<ClientDTO> findAll(Pageable pageable){
+        Page<Client> results = repository.findAll(pageable);
+        return results.map(client -> new ClientDTO(client));
     }
 }
